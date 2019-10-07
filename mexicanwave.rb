d38aa80492bc692c4f17df149fe91a518d4f9212
raj1020 <raj.1020@hotmail.com>
2019-10-07 13:07:42 +1100

    initial commit, defined the code structure of the program

---
+++ mexicanwave.rb
@@ -0,0 +1,13 @@
+
+#Create a method that takes a string, and turns it into a Mexican Wave. 
+#Ignore spaces, and notice that everything else becomes down cased!
+
+#Solution:
+
+def mexican_wave(str)
+	#Your code here
+end
+
+p mexican_wave("rabbit")
+p mexican_wave("Ole Ole Ole")
+p mexican_wave("")
