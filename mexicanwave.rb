6c2d33fa0c4bab3e8a83f5b18bbff3de74573e54
raj1020 <raj.1020@hotmail.com>
2019-10-07 13:43:14 +1100

    added codes in the method to split string and a loop

--- mexicanwave.rb
+++ mexicanwave.rb
@@ -5,7 +5,19 @@
 #Solution:
 
 def mexican_wave(str)
-	#Your code here
+    #Your code here
+    i=0
+    new_str = []
+    str = str.downcase
+str_split= str.split("")
+while i<str_split.count
+    str_split[i].upcase
+
+    i+=1
+
+end
+
+
 end
 
 p mexican_wave("rabbit")
